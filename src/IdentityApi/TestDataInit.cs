ff2823cf6ccaf0b4030aa6d199222029d24f677c
Sergiy Korzh <sergey.korzh@live.com>
2017-02-04 15:17:25 +0200

    Check if testing user exist before adding

---
+++ src/IdentityApi/TestDataInit.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityApi.Models;
+
+
+namespace IdentityApi
+{
+    public interface ITestDataInitializer {
+        Task InitTestDataAsync();
+    }
+
+    public class TestDataInitializer : ITestDataInitializer
+    {
+        private UserManager<User> _userManager;
+
+        public TestDataInitializer(UserManager<User> userManager) {
+            this._userManager = userManager;
+
+        }
+
+
+        public async Task InitTestDataAsync() {
+            var user = await _userManager.FindByNameAsync("test01@zumo.org");
+            if (user == null) {
+                await _userManager.CreateAsync(new User {
+                    Email = "test01@zumo.org",
+                    FullName = "Test User 01"
+                });
+            }
+
+            user = await _userManager.FindByNameAsync("test02@zumo.org");
+            if (user == null) {
+                await _userManager.CreateAsync(new User {
+                    Email = "test02@zumo.org",
+                    FullName = "Test User 02"
+                });
+            }
+        }
+    }
+}
