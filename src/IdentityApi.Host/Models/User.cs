66396c766b98a3c4857784426c509b0409a1b592
Sergiy Korzh <sergey.korzh@live.com>
2017-03-04 12:34:06 +0200

    Add new fields into User object

--- src/IdentityApi.Host/Models/User.cs
+++ src/IdentityApi.Host/Models/User.cs
@@ -9,31 +9,66 @@ namespace IdentityApi.Models {
             PartitionKey = DefaultPartitionKey;
             RowKey = Guid.NewGuid().ToString("N");
             DateCreated = DateTime.UtcNow;
-            FullName = "New User";
+            FirstName = "New";
+            LastName = "User";
             Roles = "";
         }
 
         public string PasswordHash { get; set; }
 
-        public string FullName { get; set; }
-
         public string Email { get; set; }
 
         public string NormalizedEmail { get; set; }
 
-        public UserGender Gender { get; set; } = UserGender.Undefined;
-
         public string Roles { get; set; }
 
         public DateTime DateCreated { get; set; }
+
+        public UserGender Gender { get; set; } = UserGender.Undefined;
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public string FaceBookUrl { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public string LinkedInUrl { get; set; }
+
+        public string TwitterUrl { get; set; }
+
+        public string YouTubeUrl { get; set; }
+
+        public string MvpUrl { get; set; }
+
+        public string WebSiteUrl { get; set; }
+
+
+        [IgnoreProperty]
+        public string FullName {
+            get {
+                string res = this.FirstName ?? "";
+                if (!string.IsNullOrEmpty(res)) {
+                    res += " ";
+                }
+                res += (this.LastName ?? "");
+                return res;
+            }
+        }
+
+
 
         [IgnoreProperty]
         public string Id {
             get { return this.RowKey; }
             set { this.RowKey = value; }
         }
-
-
     }
 
     public enum UserGender {
