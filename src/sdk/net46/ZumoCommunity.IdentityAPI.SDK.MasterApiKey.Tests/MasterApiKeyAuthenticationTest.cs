c04259286aa56be98525504ff987232cd77f602b
Anton Boyko <boyko.ant@live.com>
2017-04-14 00:08:42 +0300

    continued with master api key authentication

--- src/sdk/net46/ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests/MasterApiKeyAuthenticationTest.cs
+++ src/sdk/net46/ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests/MasterApiKeyAuthenticationTest.cs
@@ -19,6 +19,7 @@ namespace ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests
 		protected static IConfigurationProvider ValidConfigurationProvider = new ConfigurationProvider();
 		protected static string MasterApiKeyName = "MasterApiKeyName";
 		protected static string MasterApiKeyValue = "1234";
+		protected static string MasterApiKeyUrlParameter = "api_key";
 
 		[OneTimeSetUp]
 		public async Task InitializeAsync()
@@ -33,11 +34,9 @@ namespace ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests
 		{
 			get
 			{
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, MasterApiKeyName, MasterApiKeyValue);
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Anonymous, MasterApiKeyName, MasterApiKeyValue);
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Anonymous, MasterApiKeyName, string.Empty);
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Anonymous, string.Empty, MasterApiKeyValue);
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Anonymous, string.Empty, string.Empty);
+				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, MasterApiKeyName, MasterApiKeyValue);
+				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Anonymous, string.Empty, string.Empty, string.Empty);
+				yield return new TestCaseData(EmptyConfigurationProvider, AccessLevel.Anonymous, string.Empty, string.Empty, string.Empty);
 			}
 		}
 
@@ -46,18 +45,13 @@ namespace ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests
 		public async Task ValidAuthenticationAsync_Test(
 			IConfigurationProvider configurationProvider,
 			AccessLevel accessLevel,
+			string urlParameter,
 			string keyName,
 			string keyValue)
 		{
+			var httpAuthenticationContext = SetupHttpAuthenticationContext(urlParameter, keyValue);
+
 			var auth = new MasterApiKeyAuthentication(configurationProvider, accessLevel, keyName);
-
-			var httpRequestMessage = new HttpRequestMessage();
-			httpRequestMessage.RequestUri = new Uri("http://localhost/?api_key=" + keyValue);
-
-			var httpControllerContext = new HttpControllerContext { Request = httpRequestMessage };
-
-			var httpAuthenticationContext = new HttpAuthenticationContext(new HttpActionContext(httpControllerContext, new ReflectedHttpActionDescriptor()), null);
-
 			await auth.AuthenticateAsync(httpAuthenticationContext, new CancellationToken());
 
 			Assert.IsNull(httpAuthenticationContext.ErrorResult);
@@ -67,8 +61,12 @@ namespace ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests
 		{
 			get
 			{
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, MasterApiKeyName, string.Empty);
-				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, string.Empty, MasterApiKeyValue);
+				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, string.Empty, MasterApiKeyName, MasterApiKeyValue);
+				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, string.Empty, MasterApiKeyValue);
+				yield return new TestCaseData(ValidConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, MasterApiKeyName, string.Empty);
+				yield return new TestCaseData(EmptyConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, MasterApiKeyName, MasterApiKeyValue);
+				yield return new TestCaseData(EmptyConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, string.Empty, MasterApiKeyValue);
+				yield return new TestCaseData(EmptyConfigurationProvider, AccessLevel.Master, MasterApiKeyUrlParameter, MasterApiKeyName, string.Empty);
 			}
 		}
 
@@ -77,22 +75,29 @@ namespace ZumoCommunity.IdentityAPI.SDK.MasterApiKey.Tests
 		public async Task InvalidAuthenticationAsync_Test(
 			IConfigurationProvider configurationProvider,
 			AccessLevel accessLevel,
+			string urlParameter,
 			string keyName,
 			string keyValue)
 		{
+			var httpAuthenticationContext = SetupHttpAuthenticationContext(urlParameter, keyValue);
+
 			var auth = new MasterApiKeyAuthentication(configurationProvider, accessLevel, keyName);
-
-			var httpRequestMessage = new HttpRequestMessage();
-			httpRequestMessage.RequestUri = new Uri("http://localhost/?api_key=" + keyValue);
-
-			var httpControllerContext = new HttpControllerContext { Request = httpRequestMessage };
-
-			var httpAuthenticationContext = new HttpAuthenticationContext(new HttpActionContext(httpControllerContext, new ReflectedHttpActionDescriptor()), null);
-
 			await auth.AuthenticateAsync(httpAuthenticationContext, new CancellationToken());
 
 			Assert.IsNotNull(httpAuthenticationContext.ErrorResult);
 			Assert.IsNull(httpAuthenticationContext.Principal);
+		}
+
+		private HttpAuthenticationContext SetupHttpAuthenticationContext(string urlParameter, string keyValue)
+		{
+			var httpRequestMessage = new HttpRequestMessage();
+			httpRequestMessage.RequestUri = new Uri($"http://localhost/?{urlParameter}={keyValue}");
+
+			var httpControllerContext = new HttpControllerContext {Request = httpRequestMessage};
+
+			var httpAuthenticationContext =
+				new HttpAuthenticationContext(new HttpActionContext(httpControllerContext, new ReflectedHttpActionDescriptor()), null);
+			return httpAuthenticationContext;
 		}
 	}
 }
